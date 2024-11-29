3fdb2cf58d11e20f780dbd7d0d400a293313af0d
Laure Avise <marielauredon98@gmail.com>
2024-11-29 13:49:58 +0100

    Calendrier Amis update

--- script.js
+++ script.js
@@ -79,7 +79,7 @@ document.addEventListener("DOMContentLoaded", function() {
       }
 
       // Ajouter l'événement de clic
-      localStorage.setItem("flippedCards", []);
+      localStorage.removeItem("flippedCards");
       day.addEventListener("click", function() {
         if (date <= today) {
           day.classList.toggle("flipped"); // Retourne la carte
